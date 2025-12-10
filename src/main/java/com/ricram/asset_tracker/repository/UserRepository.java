b9b7c5a87648d8947a3e788cf4f2b30b3eaf3dac
Ricram <ricram.log@gmail.com>
2025-12-10 07:54:06 +0000

    Remove unused import

--- src/main/java/com/ricram/asset_tracker/repository/UserRepository.java
+++ src/main/java/com/ricram/asset_tracker/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.ricram.asset_tracker.repository;
 import com.ricram.asset_tracker.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
