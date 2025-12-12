11f3e1c9b9259e4fd99ddc527615421143312f9b
Ricram <ricram.log@gmail.com>
2025-12-12 18:03:02 +0000

    Implement get user info controller

--- src/main/java/com/ricram/asset_tracker/controller/UserController.java
+++ src/main/java/com/ricram/asset_tracker/controller/UserController.java
@@ -2,17 +2,16 @@ package com.ricram.asset_tracker.controller;
 
 import com.ricram.asset_tracker.dto.CreateUserReqDto;
 import com.ricram.asset_tracker.dto.CreateUserRespDto;
+import com.ricram.asset_tracker.dto.UserInfoDto;
 import com.ricram.asset_tracker.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -31,6 +30,15 @@ public class UserController {
                 .toUri();
         return ResponseEntity
                 .created(location)
+                .body(resp);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserInfoDto> getUserInfo(@PathVariable UUID userId) {
+        UserInfoDto resp = userService.getUserInfo(userId);
+
+        return ResponseEntity
+                .ok()
                 .body(resp);
     }
 }
